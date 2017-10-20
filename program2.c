9fa4498f8f37ef916bbb07d0151e30a0f4fe88ec
KrzysztofSnarski <krzysztof.snarski@strazgraniczna.pl>
2017-10-20 11:22:01 +0200

    nowe

---
+++ program2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int mnoz(int a, int b){
+	return a*b;
+}
+int dodaj(int a, int b){return a+b;}
+
+int main(void)
+{
+	int a =2, b= 5;/*deklaracja wraz z inicjalizacja */
+	char wybor ='*';
+
+	/* if(wyrazenie){blok jesli wyrazenie prawdziwe}
+		else {blok jesli wyrazenie falszywe}; */
+	if(wybor =='*'){
+		printf("wynik mnozenia: %i\n", mnoz(a, b));
+	}else{
+	printf("Wynik dodawaniaa: %i\n", dodaj(a, b)); /*wyswietlanie wyniku*/
+};
+	return 0;
+
+}
