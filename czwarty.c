0facd79471a6b9345b51ac75a379b48b56472540
KrzysztofSnarski <krzysztof.snarski@strazgraniczna.pl>
2017-10-20 09:20:56 +0200

    czwarty program

---
+++ czwarty.c
@@ -0,0 +1,14 @@
+#include <stdio.h> /* biblioteka standardowa input ouput */
+
+
+/* y=f(x) */
+/*    f()  */
+
+int main()  /* funkcja glowna programu */
+/* typ zwroconej wartosci (int  - wartsc calkowita */
+{
+	puts("Czwarty program" );  /* puts - funkcja biblioteki standardowej */
+					/* wyswietlajaca ciag znakow */
+return 0;
+
+}
